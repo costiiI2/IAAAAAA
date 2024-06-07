7e68160527c90dbec00152710135431d6b6c01f2
costiii2 <costantino.cecchet@heig-vd.ch>
2024-06-07 15:57:18 +0200

    ff1§5

--- lab03/iaa_students_lab03/student/code/stm32/src/main.c
+++ lab03/iaa_students_lab03/student/code/stm32/src/main.c
@@ -13,24 +13,41 @@
 #include "pm.h"
 #include "log.h"
 
-
 #include "cpx_internal_router.h"
 
-
-void receive_app_msg(const CPXPacket_t* cpxRx) {
+void receive_app_msg(const CPXPacket_t *cpxRx)
+{
 
   DEBUG_PRINT("CL FREQ = %d\n", *(int *)(cpxRx->data));
 }
 
-void appMain() {
-  DEBUG_PRINT("Waiting for activation ...\n");
-
-  vTaskDelay(M2T(300));
-  
-  cpxRegisterAppMessageHandler(receive_app_msg);
-
-  while(1) {
-    
-    vTaskDelay(M2T(100));    
+static void setVelocitySetpoint(setpoint_t *setpoint, float vx,
+                                float vy, float z, float yawrate)
+{
+  setpoint->mode.z = modeAbs;
+  setpoint->position.z = z;
+  setpoint->mode.yaw = modeVelocity;
+  setpoint->attitudeRate.yaw = yawrate;
+  setpoint->mode.x = modeVelocity;
+  setpoint->mode.y = modeVelocity;
+  setpoint->velocity.x = vx;
+  setpoint->velocity.y = vy;
+  setpoint->velocity_body = true;
+}
+void appMain()
+{
+  setpoint_t s;
+  setVelocitySetpoint(&s, 0, 0, 0.5, 0);
+  // wait for the initialization to complete
+  vTaskDelay(M2T(5000));
+  int i = 0;
+  while (1)
+  {
+    if(i++ < 10000)
+    {
+      commanderSetSetpoint(&s, 3);
+      vTaskDelay(M2T(10));
+      continue;
+    }
   }
 }
